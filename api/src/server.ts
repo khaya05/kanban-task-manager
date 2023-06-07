297b4ef721c49d59c841e83f42899f0b1af2df1e
khaya05 <khayamnyandu5@gmail.com>
2023-06-07 04:36:13 +0200

    config env file

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,11 @@
 import http from 'http';
+import { join } from 'path';
 import mongoose from 'mongoose';
 import app from './app';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: join(`${__dirname}`, 'config.env') });
+
 const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 
@@ -11,13 +13,13 @@ const connect = async () => {
   try {
     await mongoose.connect(process.env.DATABASE);
     console.log('DB connected');
+
+    server.listen(port, () => {
+      console.log(`Server running on port ${port}...`);
+    });
   } catch (error) {
     console.error(error);
   }
 };
 
 connect();
-
-server.listen(port, () => {
-  console.log(`Server running on port ${port}...`);
-});
